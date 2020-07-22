d1a64434b074f744cfc6dd614f16e878bc897c04
Mihaela <malinova29@gmail.com>
2020-07-22 22:34:20 +0300

    Add files via upload

---
+++ ProgrammingBasics/FirstStepsInCoding/src/RectangleArea.java
@@ -0,0 +1,8 @@
+public class RectangleArea {
+    public static void main (String[] args) {
+        int a = 2;
+        int b = 7;
+        int area = a * b;
+        System.out.println(area);
+    }
+}
